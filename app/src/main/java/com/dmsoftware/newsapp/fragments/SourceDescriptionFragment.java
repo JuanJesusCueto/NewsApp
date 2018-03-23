392e17116ce2854c381ec713cff845f3dc5e0749
Jesús Cueto <DMSoftware@github.com>
2018-03-23 15:18:57 -0500

    Source Description

--- app/src/main/java/com/dmsoftware/newsapp/fragments/SourceDescriptionFragment.java
+++ app/src/main/java/com/dmsoftware/newsapp/fragments/SourceDescriptionFragment.java
@@ -8,17 +8,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.androidnetworking.widget.ANImageView;
 import com.dmsoftware.newsapp.R;
 import com.dmsoftware.newsapp.activities.MainActivity;
 import com.dmsoftware.newsapp.activities.NewsActivity;
 import com.dmsoftware.newsapp.models.Source;
+import com.dmsoftware.newsapp.networking.ClarbitAPI;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SourceDescriptionFragment extends Fragment {
 
+    ANImageView logoANImageView;
+    TextView descriptionTextView;
+    TextView urlTextView;
+    TextView categoryTextView;
+    TextView languageTextView;
+    TextView countryTextView;
 
     public SourceDescriptionFragment() {
         // Required empty public constructor
@@ -33,6 +44,24 @@ public class SourceDescriptionFragment extends Fragment {
         ((NewsActivity)getActivity()).setFragmentToolbar(source.getName(),true,getFragmentManager());
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_source_description, container, false);
+
+        //Initialize variables
+        logoANImageView = (ANImageView) view.findViewById(R.id.logoDescriptionANImageView);
+        descriptionTextView = (TextView) view.findViewById(R.id.descriptionTextView);
+        urlTextView = (TextView) view.findViewById(R.id.urlTextView);
+        categoryTextView = (TextView) view.findViewById(R.id.categoryTextView);
+        languageTextView = (TextView) view.findViewById(R.id.languageTextView);
+        countryTextView = (TextView) view.findViewById(R.id.countryTextView);
+
+        //Set values
+        logoANImageView.setDefaultImageResId(R.mipmap.ic_launcher);
+        logoANImageView.setErrorImageResId(R.mipmap.ic_launcher);
+        logoANImageView.setImageUrl(ClarbitAPI.getUrlToLogo(source.getUrl()));
+        descriptionTextView.setText(source.getDescription());
+        urlTextView.setText(source.getUrl());
+        categoryTextView.setText(source.getCategory());
+        languageTextView.setText(source.getLanguage());
+        countryTextView.setText(source.getCountry());
         return view;
     }
 
